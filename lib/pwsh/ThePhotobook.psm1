4b43768d829656ae854823318dfe0d4d3c52436a
AndreasK <andreas.kirschner@gmail.com>
2022-12-12 08:15:58 +0100

    updates

--- lib/pwsh/ThePhotobook.psm1
+++ lib/pwsh/ThePhotobook.psm1
@@ -1,9 +1,11 @@
+$script:MaxPicWidth = 170
+
 class ThePhoto {
 
 	[string]$Path;
 	[int]$Width;
 	[int]$Height;
-	[bool]$Standing;
+	[bool]$Upright;
 	[datetime]$Date;
 	[string]$Comment;
 
@@ -12,7 +14,7 @@ class ThePhoto {
 		$metadata = Extract-Metadata -FilePath $Path.FullName -Raw
 		$this.Height = $this.GetTag("Image Height", $metadata)
 		$this.Width = $this.GetTag("Image Width", $metadata)
-		$this.Standing = $this.Height -gt $this.Width
+		$this.Upright = $this.Height -gt $this.Width
 		$this.Comment = $this.GetTag("User Comment", $metadata)
 		$dateString = $null `
 			?? $this.GetTag("Date.*Original", "Exif", $metadata) `
@@ -45,7 +47,7 @@ class ThePhoto {
 	}
 
 	[int] GetOptimalWidth() {
-		return [Math]::Min(110 / $this.Height * $this.Width, 160)
+		return [Math]::Min(110 / $this.Height * $this.Width, $script:MaxPicWidth)
 	}
     
 	[string] GetHumanDate() {
@@ -67,10 +69,18 @@ function Write-ThePhotos {
 	)
 	Import-MetadataExtractor
 	[System.Threading.Thread]::CurrentThread.CurrentCulture = $Culture
-	jhead -autorot (Join-Path $Path "*") *>&1 | Write-Verbose
+
+	$jheadInput = $Path
+	if ((($Path | Get-Item) -as [System.IO.DirectoryInfo])?.Exists){
+		$jheadInput = (Join-Path $Path "*")
+	}
+
+	$jheadCmd = "jhead -autorot $jheadInput *>&1"
+	$jheadCmd | Write-Verbose
+	Invoke-Expression $jheadCmd | Write-Verbose
 	$photos = @( $Path | Get-ChildItem -File | % { $p = [ThePhoto]::new($_); $p | Write-Verbose; $p } | Sort-Object -Property Date )
 
-	$neededSpace = 0
+	$neededSpace = 2
 	for ($i = 0; $i -lt $photos.Length; $i++) {
 		[ThePhoto]$pmd = $photos[$i]
 		if (-not $pmd) {
@@ -79,20 +89,18 @@ function Write-ThePhotos {
 		if ($neededSpace % 4 -eq 0) {
 			"\clearpage" 
 		}
-		if (-not ($pmd.Standing)) {
+		if (-not ($pmd.Upright)) {
 			"\photoNouveauN{$($pmd.Path | Resolve-RelativePath)}{$($pmd.GetOptimalWidth())mm}{$($pmd.GetHumanDate());$($pmd.Comment)}{}{}{}" 
 			$neededSpace += 1
 			$photos[$i] = $null
 		}
 		else {
 			$r = @()
-			# $r += ($i=$WinSize..$i-1)
-			# $r += $i
 			$r += (($i + 1)..($i + $WinSize))
 			$pairFound = $false
 			foreach ($j in $r) {
 				[ThePhoto]$other = $photos[$j]
-				if ( (-not $other) -or (-not $other.Standing)) {
+				if ( (-not $other) -or (-not $other.Upright)) {
 					continue
 				}
 				"\photoNouveauN{$($pmd.Path | Resolve-RelativePath)}{$($pmd.GetOptimalWidth())mm}{$($pmd.GetHumanDate());$($pmd.Comment)}{$($other.Path | Resolve-RelativePath)}{$($other.GetOptimalWidth())mm}{$($other.GetHumanDate());$($other.Comment)}" 
